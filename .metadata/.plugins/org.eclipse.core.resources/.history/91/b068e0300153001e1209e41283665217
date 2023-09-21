891a922040109804b68c95ac4773ea0867b62ee0
Oscar Näslund Cuesta <oscar.ncuesta@yahoo.se>
2023-09-21 23:10:52 +0200

    lab2

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/91/b068e0300153001e1209e41283665217
@@ -0,0 +1,26 @@
+package game;
+
+public class HumanPlayer extends Player {
+
+	public HumanPlayer(String userId) {
+		super(userId);
+	}
+
+	public int takePins(Board board) {
+		// reduces noPins by 1 or 2
+
+		int pinsToTake = UserInterface.askForInt("Human - how many pins do you want to take?:");
+		//int pinsToTake = scanner.nextInt();
+
+		if (pinsToTake == 1 || pinsToTake == 2) {
+			board.takePins(pinsToTake);
+					
+		} else {
+			UserInterface.printMessage("You can only take 1 or 2 pins. Try again...");
+			takePins(board); // recursive until correct input
+		}
+		
+		return pinsToTake;
+
+	}
+}
--- lab2/src/textproc/Holgersson.java
+++ lab2/src/textproc/Holgersson.java
@@ -2,7 +2,10 @@ package textproc;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Holgersson {
 
@@ -13,20 +16,47 @@ public class Holgersson {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
+		long t0 = System.nanoTime();
+		
+		ArrayList<TextProcessor> objList = new ArrayList<>();
 		TextProcessor p = new SingleWordCounter("nils");
+		objList.add(p);
+		TextProcessor p2 = new SingleWordCounter("norge");
+		objList.add(p2);
+		TextProcessor p3 = new MultiWordCounter(REGIONS);
+		objList.add(p3);
+		
+		Set<String> stopwords = new HashSet<>();
+		Scanner scan = new Scanner(new File("undantagsord.txt"));
+		scan.findWithinHorizon("\uFEFF", 1);
+		scan.useDelimiter("(\\s|,|\\.|:|;|!|\\?|'|\\\")+"); // se handledning
 
-		Scanner s = new Scanner(new File("nilsholg.txt"));
-		s.findWithinHorizon("\uFEFF", 1);
-		s.useDelimiter("(\\s|,|\\.|:|;|!|\\?|'|\\\")+"); // se handledning
-
-		while (s.hasNext()) {
-			String word = s.next().toLowerCase();
-
-			p.process(word);
+		while (scan.hasNext()) {
+			String undOrd = scan.next().toLowerCase();
+			stopwords.add(undOrd);
 		}
+			
+			
+		TextProcessor r = new GeneralWordCounter(stopwords);
+		objList.add(r);
 
-		s.close();
+		for (TextProcessor obj : objList) {
+			
+			Scanner s = new Scanner(new File("nilsholg.txt"));
+			s.findWithinHorizon("\uFEFF", 1);
+			s.useDelimiter("(\\s|,|\\.|:|;|!|\\?|'|\\\")+"); // se handledning
 
-		p.report();
+			while (s.hasNext()) {
+				String word = s.next().toLowerCase();
+
+				obj.process(word);
+			}
+			obj.report();
+			s.close();
+		}
+		
+		long t1 = System.nanoTime();
+		System.out.println("tid: " + (t1 - t0) / 1000000.0 + " ms");
+
 	}
 }
